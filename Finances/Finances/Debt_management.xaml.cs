27e7cb22f68f1fe24e921b9689df92991ccb8934
Dimitrii Bielousov <dimitrii.bielousov@gmail.com>
2018-03-19 02:51:07 +0200

    Menu list created

---
+++ Finances/Finances/Debt_management.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Finances
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Debt_management : ContentPage
+	{
+		public Debt_management ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
--- Finances/Finances/MainPage.xaml.cs
+++ Finances/Finances/MainPage.xaml.cs
@@ -13,5 +13,38 @@ namespace Finances
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void Accounts_Button_Clicked(object sender, EventArgs e)
+        {
+            NavigationPage.PushAsync(new Accounts());
+        }
+        private void Income_Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Incomes());
+        }
+        private void Expenses_Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Expenses());
+        }
+        private void Transfers_Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Transfers());
+        }
+        private void Debt_management_Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Debt_management());
+        }
+        private void Reports_Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Reports());
+        }
+        private void Settings_Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Settings());
+        }
+        private void About_Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new About());
+        }
+    }
 }
